8a93b06a18755c405530151189780ed485ef6a27
Ivor Padilla <ivorjpc@gmail.com>
2020-02-10 01:59:52 +0100

    app is now running on serverless zeit, added a show query param to limit display of prices

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const { db } = require('../config/db')
 
 module.exports = async (req, res) => {
+
+  let {show} = req.query;
+  show = Number(show);
+
   try {
 		const docs = await db.collection("dolar-promedio").get();
 		const docsArr = [];
@@ -8,9 +12,15 @@ module.exports = async (req, res) => {
 			docsArr.push(doc.data());
 		});
 
-		docsArr.sort((a, b) => new Date(b.date) - new Date(a.date));
+    docsArr.sort((a, b) => new Date(b.date) - new Date(a.date));
+    
+    if(show && typeof(show) === "number" && show > 0) {
+      res.send(docsArr.slice(0, show));
+    } else {
+      res.send(docsArr);
+    }
 
-		res.send(docsArr);
+		
 	} catch (error) {
 		res.send({ error: "error retrieving data" });
   }
--- app.js
+++ app.js
@@ -1,22 +1,33 @@
+const createError = require("http-errors");
+const express = require("express");
 const cron = require("node-cron");
-const { getDolarPromedio } = require("./helpers/dolarpromedio.js");
-const { db } = require('./config/db')
+const {db} = require('./config/db');
 
 require("dotenv").config();
+
+const app = express();
+
+app.get('/', (req, res, n) => res.send('cron job is running'))
+
 const dolarRef = db.collection("dolar-promedio");
 
 cron.schedule("0 * * * *", async () => {
   console.log(`task is running at ${new Date()} `);
-
+  
   const result = await getDolarPromedio();
 
   result.forEach(p => {
-    dolarRef.doc(p.id.toString()).set({
-      date: p.date,
-      value: p.value
-    });
-  });
+		dolarRef.doc(p.id.toString()).set({
+			date: p.date,
+			value: p.value
+		});
+	});
+  
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
 
 module.exports = app;
